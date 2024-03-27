b73057097c68888bf2cfd96005b25f348e1d6eac
Mykhailo Boiev <michaelboyev@gmail.com>
2024-03-27 20:34:41 +0100

    init commit

---
+++ src/test/java/com/mboiev/ticketapi/TicketapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mboiev.ticketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
